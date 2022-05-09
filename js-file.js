691da7f5ca8a43ebc2defe8a8860dd44ca76b57e
Kyaw Thu <evanch98@gmail.com>
2022-05-09 23:10:47 +0800

    Updating logic

--- js-file.js
+++ js-file.js
@@ -5,9 +5,22 @@ async function fetchData(city) {
 }
 
 async function times(city) {
-	let fetchTime = await fetch(`https://api.ipgeolocation.io/timezone?apiKey=9b089f1ee6ee4afcac9ac1487e31a111&location=${city}`);
+	let fetchTime = await fetch(`https://api.ipgeolocation.io/timezone?apiKey=9b089f1ee6ee4afcac9ac1487e31a111&location=${city}`, {mode: 'cors'});
 	let json = fetchTime.json();
 	return json;
+}
+
+async function processCurrent() {
+	let data = await fetchPosition();
+  let temp = data.main.temp;
+  let temp_min = data.main.temp_min;
+  let temp_max = data.main.temp_max;
+  let feels = data.main.feels_like;
+  let pressure = data.main.pressure;
+  let humidity = data.main.humidity;
+  let speed = data.wind.speed;
+  let cityName = data.name;
+  return {temp, temp_min, temp_max, feels, pressure, humidity, speed, cityName, hours};
 }
 
 async function processData(city) {
@@ -42,13 +55,31 @@ const wind = document.querySelector('.speed');
 const press = document.querySelector('.pressure');
 const body = document.querySelector('body');
 
-const hours = new Date().getHours();
-const isDay = hours > 6 && hours < 18;
-if (isDay) {
-	body.style.cssText = "background-image: url(background/morning.jpg)";
-} else {
-	body.style.cssText = "background-image: url(background/night.jpg)";
-}
+processData("New York").then (res => {
+	let hours = res.hours.split(':')[0];
+	const isDay = hours > 6 && hours < 18;
+	if (isDay) {
+		body.style.cssText = "background-image: url(background/morning.jpg)";
+	} else {
+		body.style.cssText = "background-image: url(background/night.jpg)";
+	}
+	let city = res.cityName;
+  let temp = res.temp;
+	let temp_max = res.temp_max;
+	let temp_min = res.temp_min;
+	let feels = res.feels;
+	let humidity = res.humidity;
+	let speed = res.speed;
+	let pressure = res.pressure;
+	temperature.textContent = `${parseInt(temp)}°`;
+	currentCity.textContent = city;
+	tempHigh.textContent = `High: ${parseInt(temp_max)}°`;
+	tempLow.textContent = `Low: ${parseInt(temp_min)}°`;
+	feelsLike.textContent = `Feels Like: ${parseInt(feels)}°`;
+	humid.textContent = `Humidity: ${humidity}%`;
+	wind.textContent = `Wind Speed: ${speed} m/s`;
+	press.textContent = `Pressure: ${pressure} hPa`;
+});
 
 btn.addEventListener('click', () => {
   processData(search.value)
@@ -68,7 +99,6 @@ btn.addEventListener('click', () => {
 			let humidity = res.humidity;
 			let speed = res.speed;
 			let pressure = res.pressure;
-    	console.log(temp);
 			temperature.textContent = `${parseInt(temp)}°`;
 			currentCity.textContent = city;
 			tempHigh.textContent = `High: ${parseInt(temp_max)}°`;
