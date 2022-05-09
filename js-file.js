9e36baaa29e058c92e2ff049cf326bccc8969a7a
Kyaw Thu <evanch98@gmail.com>
2022-05-09 16:45:03 +0800

    Restarting from scratch

--- js-file.js
+++ js-file.js
@@ -1,33 +1,8 @@
-'use strict';
-
-class WeatherApp {
-
-    constructor(city) {
-        this.city = city;
-    }
-
-    setCity(name) {
-        this.city = name;
-    }
-
-    #weatherData () {
-        let data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=0ba4d921bd287fe583eff55b5acd394e`);
-        return data;
-    }
-
-    async processData() {
-        let dataPromise = await this.#weatherData();
-        let dataJson = await dataPromise.json();
-        let timezone = dataJson.base;
-        return timezone;
-    }
-
+async function getData(city) {
+    let fetchData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ba4d921bd287fe583eff55b5acd394e&units=metric`, {mode: 'cors'});
+    let json = await fetchData.json();
+    console.log(json);
 }
 
-let weather = new WeatherApp("London");
-
-let data = weather.processData();
-data.then((res) => {
-    console.log(res);
-})
+getData("London");
 
